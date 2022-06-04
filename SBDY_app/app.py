1bbf5749731606010f350556c8136d2ad8ed00aa
0dminnimda <0dminnimda@gmail.com>
2022-06-04 14:26:52 +0300

    app.py: match overall look of the app (docs)

--- SBDY_app/app.py
+++ SBDY_app/app.py
@@ -1,25 +1,50 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+
+app_description = """
+Вступительное задание в Летнюю Школу Бэкенд Разработки Яндекса 2022
+"""
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app = FastAPI(
+    title="Mega Market Open API",
+    description=app_description,
+    version="1.0",
+)
 
 
-@app.get("/red")
-def read_sus():
-    return "Red is sus af, vote 'em out!"
+decs = {
+    "imports": "",
+    "delete": "",
+    "nodes": "",
+    "sales": "",
+    "statistic": "",
+}
+
+tag1 = "Базовые задачи"
+tag2 = "Дополнительные задачи"
 
 
-@app.get("/airspeed_of_a_fully_laden_swallow/{speed}")
-def read_item(speed: int):
-    answer = "Wrong!!"
-    if speed == 40:
-        answer = "Yes!! 40 kilometers per hour"
-    elif speed == 11:
-        answer = "Yes!! 11 meters per second"
-    elif speed == 24:
-        answer = "Yes!! 24 miles per hour"
-    return {"answer": answer}
+@app.post("/imports", description=decs["imports"], tags=[tag1])
+def imports():
+    return "Not implemented yet"
+
+
+@app.delete("/delete/{id}", description=decs["delete"], tags=[tag1])
+def delete():
+    return "Not implemented yet"
+
+
+@app.get("/nodes/{id}", description=decs["nodes"], tags=[tag1])
+def nodes():
+    return "Not implemented yet"
+
+
+@app.get("/sales", description=decs["sales"], tags=[tag2])
+def sales():
+    return "Not implemented yet"
+
+
+@app.get("/node/{id}/statistic", description=decs["statistic"], tags=[tag2])
+def node_statistic():
+    return "Not implemented yet"
