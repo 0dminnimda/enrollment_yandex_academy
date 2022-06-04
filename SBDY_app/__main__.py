4c303c9c90770cc4cceddb4fccc1f2735782c092
0dminnimda <0dminnimda@gmail.com>
2022-06-04 22:06:45 +0300

    Update __main__.py: use run.py

--- SBDY_app/__main__.py
+++ SBDY_app/__main__.py
@@ -1,15 +1,3 @@
-import uvicorn
-from pathlib import Path
+from SBDY_app import run
 
-
-docker = False
-
-
-uvicorn.run(
-    "__init__:app",
-    host="0.0.0.0" if docker else "localhost",
-    port=80,
-    reload=True,
-    log_level="info",
-    app_dir=str(Path(__file__).parent.absolute())
-)
+run()
