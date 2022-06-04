6725b61c4183d297ad2fb375041026212d8cb5c5
0dminnimda <0dminnimda@gmail.com>
2022-06-04 22:50:36 +0300

    app.py: make all route functions async

--- SBDY_app/app.py
+++ SBDY_app/app.py
@@ -23,25 +23,25 @@ def path_with_docs(decorator: AnyCallable, path: str) -> AnyCallable:
 
 
 @path_with_docs(app.post, "/imports")
-def imports(req: ShopUnitImportRequest):
+async def imports(req: ShopUnitImportRequest):
     return "Not implemented yet"
 
 
 @path_with_docs(app.delete, "/delete/{id}")
-def delete(id: UUID):
+async def delete(id: UUID):
     return "Not implemented yet"
 
 
 @path_with_docs(app.get, "/nodes/{id}")
-def nodes(id: UUID):
+async def nodes(id: UUID):
     return "Not implemented yet"
 
 
 @path_with_docs(app.get, "/sales")
-def sales(date: datetime):
+async def sales(date: datetime):
     return "Not implemented yet"
 
 
 @path_with_docs(app.get, "/node/{id}/statistic")
-def node_statistic(id: UUID):
+async def node_statistic(id: UUID):
     return "Not implemented yet"
