5e9aae61c7103dc69ca96e93c29bc1826690c688
0dminnimda <0dminnimda@gmail.com>
2022-06-06 00:14:00 +0300

    test_imports.py: add tests for requred(/not) fields

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -8,6 +8,7 @@ setup()
 
 
 def test_ok(client: TestClient):
+    # different amounts of items
     data = default(ImpRequest, items=[])
     response = client.post("/imports", data=data.json())
     assert response.status_code == 200
@@ -20,15 +21,46 @@ def test_ok(client: TestClient):
     response = client.post("/imports", data=data.json())
     assert response.status_code == 200
 
+    # not required fields
+    data = default(ImpRequest)
+    del data.items[0].parentId
+    response = client.post("/imports", data=data.json())
+    assert response.status_code == 200
+
+    data = default(ImpRequest)
+    del data.items[0].price
+    response = client.post("/imports", data=data.json())
+    assert response.status_code == 200
+
 
 def test_validation(client: TestClient):
-    data = default(ImpRequest, items=[]).json(exclude={"items"})
-    response = client.post("/imports", data=data)
+    # required fields in ImpRequest
+    string = default(ImpRequest, items=[]).json(exclude={"items"})
+    response = client.post("/imports", data=string)
     assert response.status_code == 400
     assert response.json() == ERROR_400
 
-    data = default(ImpRequest).json(exclude={"updateDate"})
-    response = client.post("/imports", data=data)
+    string = default(ImpRequest).json(exclude={"updateDate"})
+    response = client.post("/imports", data=string)
+    assert response.status_code == 400
+    assert response.json() == ERROR_400
+
+    # required fields in Import
+    data = default(ImpRequest)
+    del data.items[0].id
+    response = client.post("/imports", data=data.json())
+    assert response.status_code == 400
+    assert response.json() == ERROR_400
+
+    data = default(ImpRequest)
+    del data.items[0].name
+    response = client.post("/imports", data=data.json())
+    assert response.status_code == 400
+    assert response.json() == ERROR_400
+
+    data = default(ImpRequest)
+    del data.items[0].type
+    response = client.post("/imports", data=data.json())
     assert response.status_code == 400
     assert response.json() == ERROR_400
 
