3426fd5da3240207c32d2f293f8e86b2c539c8a0
Андрей Зворыгин <don-dron@yandex-team.ru>
2022-06-06 11:50:09 +0000

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -31,7 +31,7 @@ IMPORT_BATCHES = [
                 "type": "CATEGORY",
                 "name": "Смартфоны",
                 "id": "d515e43f-f3f6-4471-bb77-6b455017a2d2",
-                "parentId": "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1",
+                "parentId": "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1"
             },
             {
                 "type": "OFFER",
@@ -56,7 +56,7 @@ IMPORT_BATCHES = [
                 "type": "CATEGORY",
                 "name": "Телевизоры",
                 "id": "1cc0129a-2bfe-474c-9ee6-d435bf5fc8f2",
-                "parentId": "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1",
+                "parentId": "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1"
             },
             {
                 "type": "OFFER",
