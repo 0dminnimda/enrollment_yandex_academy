43d8bc0bb58ca6850f8a7c80984d8fd1cc1e100b
0dminnimda <0dminnimda@gmail.com>
2022-06-09 16:22:59 +0300

    patches.py: move imports inside the functions

--- SBDY_app/patches.py
+++ SBDY_app/patches.py
@@ -2,8 +2,6 @@ from datetime import datetime
 from typing import Any
 
 from ciso8601 import parse_datetime, parse_rfc3339
-from pydantic.errors import DateTimeError
-from pydantic.validators import _VALIDATORS
 
 
 def strict_datetime(value: Any) -> datetime:
@@ -15,10 +13,14 @@ def strict_datetime(value: Any) -> datetime:
         # raises ValueError if string is invalid datetime
         return parse_datetime(value)  # parse_rfc3339(v)
     except Exception as e:
+        from pydantic.errors import DateTimeError
+
         raise DateTimeError() from e
 
 
 def patch_datetime_validation():
+    from pydantic.validators import _VALIDATORS
+
     for i, (tp, _) in enumerate(_VALIDATORS):
         if tp == datetime:
             _VALIDATORS[i] = (tp, [strict_datetime])
