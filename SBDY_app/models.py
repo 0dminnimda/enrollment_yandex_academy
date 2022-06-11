17106a21870eb5ebc100ee2a829cd3d76cbf2dde
0dminnimda <0dminnimda@gmail.com>
2022-06-11 22:29:56 +0300

    Create SBDY_app\models.py

---
+++ SBDY_app/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeMeta, declarative_base, relationship
+# from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy_utils import UUIDType
+
+from .schemas import ShopUnitType
+
+
+Base: DeclarativeMeta = declarative_base()
+
+
+class Depth(Base):
+    __tablename__ = 'depth'
+
+    id = Column(Integer, primary_key=True, unique=True)
+
+    depth = Column(Integer)
+
+
+class ShopUnit(Base):
+    __tablename__ = 'shop'
+
+    id = Column(UUIDType(), primary_key=True, unique=True)
+    parentId = Column(UUIDType(), ForeignKey('shop.id'), nullable=True)
+    children = relationship("ShopUnit")  # , lazy='selectin')
+
+    name = Column(String)
+    type = Column(Enum(ShopUnitType))
+    price = Column(Integer, nullable=True)
+    date = Column(DateTime)
