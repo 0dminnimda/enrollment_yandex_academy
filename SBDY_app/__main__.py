c6fb8f97f685b665c2fb4573115bb4f3d1b278ee
0dminnimda <0dminnimda@gmail.com>
2022-06-13 20:46:47 +0300

    __main__.py: add argparser with docker option

--- SBDY_app/__main__.py
+++ SBDY_app/__main__.py
@@ -1,3 +1,17 @@
+"""
+Main command-line interface for the application
+"""
+
+import argparse
+
 from SBDY_app import run
 
-run()
+parser = argparse.ArgumentParser(
+    prog="SBDY_app", description="Run web app that simulates an online shop")
+parser.add_argument("-d", "--docker", action="store_true",
+                    help="host app on 0.0.0.0 instead of the localhost")
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    run(docker=args.docker)
