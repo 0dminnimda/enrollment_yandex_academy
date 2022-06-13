b6134d2e24414735e1f908dc69dcd8a93ffd43b0
0dminnimda <0dminnimda@gmail.com>
2022-06-14 02:20:53 +0300

    Create SBDY_app\options.py

---
+++ SBDY_app/options.py
@@ -0,0 +1,9 @@
+"""
+Application-wide settings and options
+"""
+
+# by default we don't reload, normal production mode
+RELOAD: bool = False
+
+# by default we run in production mode
+DEV_MODE: bool = False
