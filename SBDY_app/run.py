696b238544b0664dfcb8cbc9fa0e110c772a3804
0dminnimda <0dminnimda@gmail.com>
2022-06-14 02:22:03 +0300

    run.py: use options.py, fix comment

--- SBDY_app/run.py
+++ SBDY_app/run.py
@@ -8,11 +8,11 @@ from pathlib import Path
 
 import uvicorn
 
+from SBDY_app import options
 from SBDY_app.app import app  # for uvicorn.run
 
 
-RELOAD = False  # by default we don't reload, normal production mode
-DEBUGGING = 'debugpy' in sys.modules
+DEBUGGING = "debugpy" in sys.modules
 
 
 def run(host: str = "localhost") -> None:
@@ -21,17 +21,19 @@ def run(host: str = "localhost") -> None:
         f"{file.stem}:app",
         host=host,
         port=80,
-        reload=RELOAD,
+        reload=options.RELOAD,
         log_level="info",
         app_dir=str(file.parent.absolute())
     )
 
 
 if __name__ == "__main__":
-    # but if this file is launched directly, this is a sign that it is a dev,
-    # therefore, if we do not debugging, which requires RELOAD == False,
+    options.DEV_MODE = True
+
+    # if this file is launched directly, this is a sign that it is a dev,
+    # therefore, if we are not debugging, which requires RELOAD == False,
     # we set the RELOAD to True to simplify the development
     if not DEBUGGING:
-        RELOAD = True
+        options.RELOAD = True
 
     run()
