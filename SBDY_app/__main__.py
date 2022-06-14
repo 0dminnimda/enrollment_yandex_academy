4781c8236dbecb9c7f605fd99e3741384adbe103
0dminnimda <0dminnimda@gmail.com>
2022-06-14 11:11:34 +0300

    __main__.py: format

--- SBDY_app/__main__.py
+++ SBDY_app/__main__.py
@@ -6,6 +6,7 @@ import argparse
 
 from SBDY_app import run
 
+
 parser = argparse.ArgumentParser(
     prog="SBDY_app", description="Run web app that simulates an online shop")
 parser.add_argument("--host", default="localhost",
@@ -13,6 +14,7 @@ parser.add_argument("--host", default="localhost",
                     " default is 'localhost'")
 
 args = parser.parse_args()
+
 
 if __name__ == "__main__":
     run(host=args.host)
