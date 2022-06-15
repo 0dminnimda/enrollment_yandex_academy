4edd7f5bf8325a822952bf8b7cc6f342171c7392
0dminnimda <0dminnimda@gmail.com>
2022-06-15 13:49:44 +0300

    test_delete.py: add and fix tests

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -1,5 +1,5 @@
 from uuid import UUID
-from SBDY_app.schemas import ImpRequest, Import, ShopUnitType
+from SBDY_app.schemas import ImpRequest, Import, ShopUnit, ShopUnitType
 
 from utils import ERROR_400, ERROR_404, Client, client, default, do_test, setup
 
@@ -33,7 +33,9 @@ def test_delete_hierarchy(client: Client):
     items = [
         default(Import, id=id1, parentId=None,
                 type=ShopUnitType.CATEGORY, price=None),
-        default(Import, id=id2, parentId=id1)]
+        default(Import, id=id2, parentId=id1,
+                type=ShopUnitType.OFFER)]
+    items[0].price = None
     data = default(ImpRequest, items=items)
     response = client.imports(data.json())
     assert response.status_code == 200
@@ -59,6 +61,30 @@ def test_nonexisting_items(client: Client):
     assert response.json() == ERROR_404
 
 
+def test_parent_change(client: Client):
+    id1, id2 = default(UUID), default(UUID)
+    items = [
+        default(Import, id=id1, parentId=None,
+                type=ShopUnitType.CATEGORY, price=None),
+        default(Import, id=id2, parentId=id1,
+                type=ShopUnitType.OFFER, price=42069)]
+    items[0].price = None
+    data = default(ImpRequest, items=items)
+    response = client.imports(data.json())
+    assert response.status_code == 200
+
+    response = client.nodes(id1)
+    assert response.status_code == 200
+    assert ShopUnit(**response.json()).price == 42069
+
+    response = client.delete(id2)
+    assert response.status_code == 200
+
+    response = client.nodes(id1)
+    assert response.status_code == 200
+    assert ShopUnit(**response.json()).price is None
+
+
 def test_validation(client: Client):
     response = client.delete("abooba")
     assert response.status_code == 400
@@ -71,9 +97,6 @@ def test_validation(client: Client):
     response = client.delete("3fa85f64-5717-4562-b3fc")
     assert response.status_code == 400
     assert response.json() == ERROR_400
-
-
-# TODO: removing category removes its children
 
 
 if __name__ == "__main__":
