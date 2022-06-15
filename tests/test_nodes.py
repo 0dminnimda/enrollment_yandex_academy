84889ea4016be4707cb084e37989df740229b3e2
0dminnimda <0dminnimda@gmail.com>
2022-06-15 16:53:58 +0300

    test_nodes.py: fix and add tests

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,6 +1,8 @@
+import json
 from uuid import UUID
 
-from SBDY_app.schemas import ShopUnit
+from SBDY_app.patches import serialize_datetime
+from SBDY_app.schemas import Import, ImpRequest, ShopUnit, ShopUnitType
 
 from utils import ERROR_400, ERROR_404, Client, client, default, do_test, setup
 
@@ -9,41 +11,75 @@ setup()
 
 def test_unit(client: Client):
     id = default(UUID)
-    # TODO: adding the unit with id to the db
+    data = default(ImpRequest, items=[default(Import, id=id, parentId=None)])
+    response = client.imports(data.json())
+    assert response.status_code == 200
+
+    model = {**json.loads(data.items[0].json()),
+             "date": serialize_datetime(data.updateDate), "children": None}
 
     response = client.nodes(id)
     assert response.status_code == 200
-    # TODO: check that the item is the same
-    thing = ShopUnit(**response.json())  # no ValidationError
+    ShopUnit(**response.json())  # no ValidationError
+    assert response.json() == model
 
-    # TODO: check that the unit is still there
-    # unchanged and matches the response
+    response = client.nodes(id)
+    assert response.status_code == 200
+    ShopUnit(**response.json())  # no ValidationError
+    assert response.json() == model
 
 
 def test_empty_category(client: Client):
     id = default(UUID)
-    # TODO: adding the category without children with id to the db
+    data = default(ImpRequest, items=[default(
+        Import, id=id, parentId=None, type=ShopUnitType.CATEGORY, price=None)])
+    data.items[0].price = None
+    response = client.imports(data.json())
+    assert response.status_code == 200
+
+    model = {**json.loads(data.items[0].json()),
+             "date": serialize_datetime(data.updateDate), "children": []}
 
     response = client.nodes(id)
     assert response.status_code == 200
-    # TODO: check that the item is the same
-    thing = ShopUnit(**response.json())  # no ValidationError
+    ShopUnit(**response.json())  # no ValidationError
+    assert response.json() == model
 
-    # TODO: check that the unit is still there
-    # unchanged and matches the response
+    response = client.nodes(id)
+    assert response.status_code == 200
+    ShopUnit(**response.json())  # no ValidationError
+    assert response.json() == model
 
 
 def test_full_category(client: Client):
     id = default(UUID)
-    # TODO: adding the category with children with id to the db
+    items = [
+        default(Import, id=id, parentId=None,
+                type=ShopUnitType.CATEGORY, price=None),
+        default(Import, parentId=id,
+                type=ShopUnitType.OFFER, price=0),
+        default(Import, parentId=id,
+                type=ShopUnitType.OFFER, price=0)]
+    items[0].price = None
+    data = default(ImpRequest, items=items)
+    response = client.imports(data.json())
+    assert response.status_code == 200
+
+    date = serialize_datetime(data.updateDate)
+    model = {**json.loads(items[0].json()), "date": date, "children": [
+        {**json.loads(items[1].json()), "date": date, "children": None},
+        {**json.loads(items[2].json()), "date": date, "children": None},
+    ], "price": 0}
 
     response = client.nodes(id)
     assert response.status_code == 200
-    # TODO: check that the item is the same
-    thing = ShopUnit(**response.json())  # no ValidationError
+    ShopUnit(**response.json())  # no ValidationError
+    assert response.json() == model
 
-    # TODO: check that the unit is still there
-    # unchanged and matches the response
+    response = client.nodes(id)
+    assert response.status_code == 200
+    ShopUnit(**response.json())  # no ValidationError
+    assert response.json() == model
 
 
 def test_nonexisting_items(client: Client):
