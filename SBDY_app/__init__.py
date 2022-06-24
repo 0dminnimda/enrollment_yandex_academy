d99a4a239b3ba0db87f53db8813481d28ee4fa42
0dminnimda <0dminnimda@gmail.com>
2022-06-24 21:20:10 +0300

    __init__.py: remove names

--- SBDY_app/__init__.py
+++ SBDY_app/__init__.py
@@ -5,9 +5,11 @@ __name__ = "SBDY_app"
 from .patches import patch
 
 patch()  # order is important
+del patch
 
 from .app import app
 from . import logger
 from .run import run
 
 logger.setup(__name__)
+del logger
