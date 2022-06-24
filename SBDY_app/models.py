1644392f2f27dc9aedc15179e538b535aab9816e
0dminnimda <0dminnimda@gmail.com>
2022-06-24 12:02:57 +0300

    models.py: remove relationship, update parentId accordingly

--- SBDY_app/models.py
+++ SBDY_app/models.py
@@ -27,9 +27,8 @@ class ShopUnit(Base):
 
     id: UUID = Column(UUIDType(), primary_key=True)  # type: ignore
     parentId: Optional[UUID] = Column(  # type: ignore
-        UUIDType(), ForeignKey("shop.id", ondelete="CASCADE"), nullable=True)
-    children: List[ShopUnit] = relationship(  # type: ignore
-        "ShopUnit", cascade="all, delete", passive_deletes=True)
+        UUIDType(), ForeignKey("shop.id"), nullable=True)
+    children: List[ShopUnit]
 
     name: str = Column(String)  # type: ignore
     date: datetime = Column(DateTime)  # type: ignore
