ae519db230c3345e79f578f7bc2f3c478dea0560
0dminnimda <0dminnimda@gmail.com>
2022-06-24 12:05:02 +0300

    typedefs.py: add ShopUnits

--- SBDY_app/typedefs.py
+++ SBDY_app/typedefs.py
@@ -1,9 +1,14 @@
-from typing import Any, Callable, Type, TypeVar
+from typing import TYPE_CHECKING, Any, Callable, Dict, Type, TypeVar
+from uuid import UUID
 
 from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession as DB
+
+if TYPE_CHECKING:
+    from .models import ShopUnit
 
 
 AnyCallable = Callable[..., Any]
 BaseModelT = TypeVar("BaseModelT", bound=Type[BaseModel])
 T = TypeVar("T", bound=Any)
+ShopUnits = Dict[UUID, "ShopUnit"]
